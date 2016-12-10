84faa8d8cd0bf97a26820d9aac7ed5ed2ec38295
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-10 16:09:28 -0500

    first commit

---
+++ Release/spiwavwin32imgstatic2.bat
@@ -0,0 +1,4 @@
+rem start spiwavwin32imgstatic2.exe 600 300 700 500 150 0 24
+rem start spiwavwin32imgstatic2.exe 600 300 700 500 150 0 36
+rem start spiwavwin32imgstatic2.exe 600 300 700 500 150 0 0 0 12
+start spiwavwin32imgstatic2.exe 600 300 700 500 220 1 0 0 12
